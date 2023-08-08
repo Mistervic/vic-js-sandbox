d087b88cafcbb94c027760f2125edc2cb697249a
Victor <valfonzom@gmail.com>
2023-08-07 22:53:27 -0400

    vic-js-sandbox

--- IntroduccionJS/js/app.js
+++ IntroduccionJS/js/app.js
@@ -1,1 +1,28 @@
 alert("Hola Mundo")
+
+// variables
+
+var producto = 'Audifonos Gamer'; // Iniciarvariable y daler valor
+
+var disponible; // Variable  sin valor
+
+producto = true; // REasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+// iniciar varias variables
+
+// var 1disponible; no permitido iniciar con numero
+// var -disponible; no permitido empezar con guion -
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD' // underscore
+var nombreProducto = 'Monitor HD' // camecase
+var Nombreproducto = 'Monitor HD' // Pascal Case
+var nombreproducto = 'Monitor HD' // lowercase
+
+console.log(producto);
+
